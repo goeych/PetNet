dc2f7273e8dad8f293708e32cb87f69a3bb6ea21
goeych <110361567+goeych@users.noreply.github.com>
2023-05-23 17:01:53 +0800

    Add files via upload

--- store/views.py
+++ store/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render,get_object_or_404
-
+from django.db.models import Q
 from .models import Product,Category
 # Create your views here.
 
+
+def search(request):
+    query = request.GET.get('query','')
+
+    products = Product.objects.filter(Q(title__icontains = query)
+                                      |Q(description__icontains=query))
+
+    context={'query':query,'products':products}
+    return render(request,'store/search.html',context)
 
 def category_detail(request,slug):
     category = get_object_or_404(Category,slug=slug)
@@ -16,6 +25,9 @@ def category_detail(request,slug):
 
 def product_detail(request,category_slug,slug):
     product = get_object_or_404(Product,slug=slug)
+    print('product.user.id',product.user.id)
+    print('product.description',product.description)
+    print('product.category.id',product.category.id)
     context={'product':product}
     return render(request,'store/product_detail.html',context)
 
