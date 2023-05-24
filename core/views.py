8d202dd1b6b2c0ce4a9af0813540912987e634c8
goeych <110361567+goeych@users.noreply.github.com>
2023-05-24 17:20:15 +0800

    Add files via upload

--- core/views.py
+++ core/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from django.db.models import Q
 
 from store.models import Product
 
 # Create your views here.
 
 def frontpage(request):
-    products = Product.objects.all()[0:6]
+    #products = Product.objects.exclude(status=Product.DECOMMISSION)
+    products = Product.objects.filter(~Q(status=Product.DECOMMISSION))
 
     context={'products':products}
     return render(request,'core/frontpage.html',context)
